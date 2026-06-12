214140d3fa4a4821324e4769d8768c027f1908f8
CppDevVS2026 <242948159+CppDevVS2026@users.noreply.github.com>
2026-06-12 22:00:43 +0200

    refactor: use `const` for local variables and add debug message in `eye_dir_open`

--- src/the_eye/the_eye.c
+++ src/the_eye/the_eye.c
@@ -9,7 +9,7 @@
 #include <windows.h>
 
 void eye_dir_open(const wchar_t *eye_szDirectoryPath) {
-    HANDLE eye_hDirectory = CreateFileW(
+    const HANDLE eye_hDirectory = CreateFileW(
         eye_szDirectoryPath,
         FILE_LIST_DIRECTORY,
         FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
@@ -20,15 +20,20 @@ void eye_dir_open(const wchar_t *eye_szDirectoryPath) {
     );
 
     if (eye_hDirectory == INVALID_HANDLE_VALUE) {
-        DWORD eye_dwError = GetLastError();
+        const DWORD eye_dwError = GetLastError();
         LPWSTR eye_szMsgBuf = NULL;
+
         FormatMessageW(
             FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
             NULL, eye_dwError, 0,
             (LPWSTR) &eye_szMsgBuf, 0, NULL);
+
         wprintf(L"Error %lu: %s\n", eye_dwError, eye_szMsgBuf);
+
         LocalFree(eye_szMsgBuf);
         CloseHandle(eye_hDirectory);
     }
+
+    wprintf(L"[Eye] Opened: %s\n", eye_szDirectoryPath);
     CloseHandle(eye_hDirectory);
 }
