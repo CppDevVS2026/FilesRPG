9527a8ebe535f596e0b6ccae7a1041924beeb271
CppDevVS2026 <242948159+CppDevVS2026@users.noreply.github.com>
2026-06-12 22:35:49 +0200

    fix: handle null directory path in `eye_dir_open` and improve error reporting

--- src/the_eye/the_eye.c
+++ src/the_eye/the_eye.c
@@ -8,7 +8,26 @@
 
 #include <windows.h>
 
+/**
+ * @brief Opens a directory for file system changes using Win32 API
+ * @param eye_szDirectoryPath directory path to open
+ * @return void
+ */
 void eye_dir_open(const wchar_t *eye_szDirectoryPath) {
+    if (eye_szDirectoryPath == NULL) {
+        const DWORD eye_dwError = GetLastError();
+        LPWSTR eye_lpMessageBuffer = NULL;
+
+        FormatMessageW(
+            FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+            NULL, eye_dwError, 0,
+            (LPWSTR) &eye_lpMessageBuffer, 0, NULL);
+
+        wprintf(L"[Eye] Error %lu: %s\n", eye_dwError, eye_lpMessageBuffer);
+        LocalFree(eye_lpMessageBuffer);
+        return;
+    }
+
     const HANDLE eye_hDirectory = CreateFileW(
         eye_szDirectoryPath,
         FILE_LIST_DIRECTORY,
@@ -28,12 +47,12 @@ void eye_dir_open(const wchar_t *eye_szDirectoryPath) {
             NULL, eye_dwError, 0,
             (LPWSTR) &eye_szMsgBuf, 0, NULL);
 
-        wprintf(L"Error %lu: %s\n", eye_dwError, eye_szMsgBuf);
+        wprintf(L"[Eye] Error %lu: %s\n", eye_dwError, eye_szMsgBuf);
 
         LocalFree(eye_szMsgBuf);
         CloseHandle(eye_hDirectory);
+    } else {
+        wprintf(L"[Eye] Opened: %s\n", eye_szDirectoryPath);
+        CloseHandle(eye_hDirectory);
     }
-
-    wprintf(L"[Eye] Opened: %s\n", eye_szDirectoryPath);
-    CloseHandle(eye_hDirectory);
 }
