ef8449d94f4f495fa2b1a6324623637b0625b3c2
CppDevVS2026 <242948159+CppDevVS2026@users.noreply.github.com>
2026-06-12 21:52:48 +0200

    feat: add `eye_dir_open` function for opening directories with wide-char support on Windows

--- apps/game_launcher/main.c
+++ apps/game_launcher/main.c
@@ -5,10 +5,12 @@
 // ============================================================================
 
 #include <stdio.h>
-
+#include "the_eye.h"
 
 int main(void) {
     wprintf(L"FilesRPG v0.0.0 — engine starting...");
+    eye_dir_open(L"C:\\Users\\JcTheKing\\CLionProjects\\FilesRPG");
+
 
     return 0;
 }
--- src/the_eye/the_eye.c
+++ src/the_eye/the_eye.c
@@ -5,3 +5,28 @@
 // ============================================================================
 
 #include "the_eye.h"
+
+#include <windows.h>
+
+void eye_dir_open(const wchar_t *eye_szDirectoryPath) {
+    HANDLE eye_hDirectory = CreateFileW(
+        eye_szDirectoryPath,
+        FILE_LIST_DIRECTORY,
+        FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+        NULL,
+        OPEN_EXISTING,
+        FILE_FLAG_BACKUP_SEMANTICS,
+        NULL
+    );
+
+    if (eye_hDirectory == INVALID_HANDLE_VALUE) {
+        DWORD eye_dwError = GetLastError();
+        LPWSTR eye_szMsgBuf = NULL;
+        FormatMessageW(
+            FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+            NULL, eye_dwError, 0,
+            (LPWSTR) &eye_szMsgBuf, 0, NULL);
+        wprintf(L"Error %lu: %s\n", eye_dwError, eye_szMsgBuf);
+        LocalFree(eye_szMsgBuf);
+    }
+}
